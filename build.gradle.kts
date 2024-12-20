e6936f877893fd64afd4101b7d11df28786c6f0d
Jesse Stricker <git@jessestricker.de>
2024-12-20 15:06:38 +0100

    feat: define Gradle publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    `maven-publish`
 
     alias(libs.plugins.detekt)
     alias(libs.plugins.kover)
@@ -23,11 +24,33 @@ kotlin {
     }
 }
 
+java {
+    withSourcesJar()
+}
+
 testing {
     suites.named<JvmTestSuite>("test") {
         useJUnitJupiter(libs.versions.junit.jupiter)
         dependencies {
             implementation(libs.kotest.assertions.core)
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("main") {
+            from(components["java"])
         }
     }
 }
