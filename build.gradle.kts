7d61af9ee28164d9646b8141d2e78151274a9881
Jesse Stricker <978183+jessestricker@users.noreply.github.com>
2024-12-20 15:22:49 +0100

    feat: release workflow (#2)
    * feat: add WIP release workflow
    
    * feat: add semantic-release
    
    * feat: add GITHUB_TOKEN to release step
    
    * feat: add Gradle publish step to semantic-release
    
    * feat: define Gradle publishing
    
    * feat: remove dry-run flag from semantic-release
    
    * fix: set groupId
    
    * fix: add missing release workflow permissions
    
    * chore: prepare for merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,14 @@
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    `maven-publish`
 
     alias(libs.plugins.detekt)
     alias(libs.plugins.kover)
     alias(libs.plugins.ktfmt)
 }
+
+group = "org.example"
 
 repositories {
     mavenCentral()
@@ -23,11 +26,33 @@ kotlin {
     }
 }
 
+java {
+    withSourcesJar()
+}
+
 testing {
     suites.named<JvmTestSuite>("test") {
         useJUnitJupiter(libs.versions.junit.jupiter)
         dependencies {
             implementation(libs.kotest.assertions.core)
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("main") {
+            from(components["java"])
         }
     }
 }
